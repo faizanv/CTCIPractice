6d3ab374dc6a4af9f16cb707a806003bebecea91
faizanv <faizan.virani.44@gmail.com>
2017-09-16 00:39:16 -0700

    Finished 2.6

--- Chapter2.java
+++ Chapter2.java
@@ -5,13 +5,59 @@ public class Chapter2 {
     MyLinkedList list = new MyLinkedList();
     list.add(2);
     list.add(1);
-    list.add(6);
-    list.add(4);
-    list.add(5);
     list.add(3);
+    list.add(3);
+    list.add(6);
+    list.add(1);
+    list.add(2);
     System.out.println(list);
-    partition(list.head, 4);
-    System.out.println(list);
+    System.out.println(isPalindromeStack(list.head)); 
+  }
+
+  // 2.6 with stack
+  public static boolean isPalindromeStack(Node node) {
+    Node fast = node;
+    Node slow = node;
+    Stack<Integer> stack = new Stack<>();
+
+    // push first half onto stack
+    while (fast != null && fast.next != null) {
+      stack.push(slow.data);
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    // odd length list
+    if (fast != null) {
+      slow = slow.next;
+    }
+
+    while (slow != null) {
+      if (slow.data != stack.pop()) {
+        return false;
+      }
+      slow = slow.next;
+    }
+    return true;
+  }
+
+  // 2.6 reverse and clone technique
+  public static boolean isPalindrome(Node node) {
+    Node cur = node;
+    Node reversedList = null;
+    while (cur != null) {
+      Node newNode = new Node(cur.data);
+      newNode.next = reversedList;
+      reversedList = newNode;
+      cur = cur.next;
+    }
+    // check if reversedList == node
+    while (node != null && reversedList != null) {
+      if (node.data != reversedList.data) return false;
+      node = node.next;
+      reversedList = reversedList.next;
+    }
+    return true;
   }
 
   // 2.4 Partition list around x
